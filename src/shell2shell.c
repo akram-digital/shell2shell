2efbe83e94220a1fa0e1dc4b624e3d073ede166b
akram-digital <wakaakrch2014@gmail.com>
2015-04-04 18:32:38 +0100

    Update shell2shell.c

--- src/shell2shell.c
+++ src/shell2shell.c
@@ -16,9 +16,6 @@ char tcode[1024]="\0";
 char compile[1024];
 char clean[1024];
 char swapargv[1024];
-printf("\n   Shell2shell v 0.1 By Akram Abed-Eslem Chaima\n ");
-printf("   under : GPL \n");
-printf("------------------------------------------------\n\n");
 if(argc<2)
 {
 printf("Usage: shell2shell -f base_command arg1... -t your_command  \n\n");
@@ -43,7 +40,7 @@ t++;
 fprintf(fp,"\n #include<stdio.h>\n #include<stdlib.h> \n int main() \n {\n system(\"%s\"); \n }",tcode);
 fclose(fp);
 printf("   *Compiling ....\n");
-snprintf(compile,sizeof(compile),"gcc %s -o %s",fcode,argv[n-1]);
+snprintf(compile,sizeof(compile),"cc %s -o %s",fcode,argv[n-1]);
 system(compile);
 printf("   *Cleaning ....\n"); 
 snprintf(clean,sizeof(clean),"mv %s /usr/bin/ && rm %s",argv[n-1],fcode);
